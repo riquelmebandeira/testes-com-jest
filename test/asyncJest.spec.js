00eb70c1df11c854e5ff0f691b3784fd211aba1b
Riquelme <riquelmebandeira1@outlook.com>
2021-08-20 14:42:33 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -7,7 +7,7 @@ Dependendo do parâmetro o retorno da função varia, veja a função no arquivo
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
 
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+ATENÇÃO! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('1 - O retorno do telefonema', () => {
